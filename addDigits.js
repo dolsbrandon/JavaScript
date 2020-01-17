699829716e13a70195bb5c3dfc8d253ac65c1fe5
dolsbrandon <59302047+dolsbrandon@users.noreply.github.com>
2020-01-16 21:06:38 -0600

    Update addDigits.js

--- addDigits.js
+++ addDigits.js
@@ -1,16 +1,43 @@
-// Convert Int to String
-let num = 7894
-console.log('Num is: ' + num.toString());
-console.log('Num length is: ' + num.toString().length);
+/*
+Solution for LeetCode Problem: Add Digits
+Created by Brandon Dols
 
+Notes from LeetCode:
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+Since 2 has only one digit, return it.
+*/
+
+// Example Number
+let num = 789415641
+
+// Track Loop Count
+let numLoops = 0
+
+// Check if solution reached
 while (num.toString().length > 1) {
+
+    // Get number of digits in the number
     let numLength = num.toString().length;
     let newNum = 0;
+
+    // Loop through current number and add digits
     for (let count = numLength; count > 0; count--) {
         let currentNum = Math.floor((num / Math.pow(10, count - 1)) % 10);
         newNum += currentNum;
-        console.log('Current Num is: ' + currentNum);
     }
-    console.log('End of for loop result: ' + newNum);
+    
+    // Log current loop number and result
+    numLoops++;
+    console.log('Loop ' + numLoops + ' result: ' + newNum);
     num = newNum;
 }
+
+// Log final result
+console.log('Solution is: ' + num);
