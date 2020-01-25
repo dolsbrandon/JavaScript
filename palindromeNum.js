95773afc45f5d944658b3f6df9db96a633b78cfe
dolsbrandon <59302047+dolsbrandon@users.noreply.github.com>
2020-01-25 17:53:46 -0600

    update

--- palindromeNum.js
+++ palindromeNum.js
@@ -1,19 +1,36 @@
+/*
+Palindrome Number by Brandon Dols
+
+Notes: Tests number for palindrome
+*/
+
+// palindrome test function
 var isPalindrome = function(x) {
+
+    // return false if negative
     if(x < 0) {
         return false;
     }
+
+    // store reverse number
     let revNum = 0;
+
+    // number length in digits
     let numLen = Math.ceil(Math.log10(x + 1));
+
+    // iterate through digits of number and create reverse number
     for(i = 0; i < numLen; i++) {
         revNum += (Math.floor((x / 10**i) % 10) * 10**(numLen-(i+1)));
     }
+    // is palindrome
     if (revNum == x) {
         console.log(`${x} is a palindrome`);
     }
+    // is not palindrome
     else {
         let str = 
         console.log(`${x} is not a palindrome`);
     }
 };
 
-isPalindrome(41567);
+isPalindrome(415676514);
