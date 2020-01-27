0def2bfa77ee6cf2afdd59bef38fbd926d6ffb90
dolsbrandon <59302047+dolsbrandon@users.noreply.github.com>
2020-01-26 21:37:45 -0600

    update

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,11 +1,22 @@
+/*
+Remove Duplicates by Brandon Dols
+
+LeetCode Notes: Given a sorted array nums, remove the duplicates
+in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this
+by modifying the input array in-place with O(1) extra memory.
+*/
+
 var removeDuplicates = function(nums) {
     let length = nums.length;
     let origArr = [...nums];
+
     for(i = 0; i < length; i++){
         if(nums[i] == nums[i+1]) {
             nums.splice(i,1);
             length -=1;
-            i-=1;
+            i-=1; // account for multiple duplicates
         }
     }
     console.log(`\n[Duplicates Successfully Removed]`);
